629432a19da2cd3b93f3eee0f8923e2f4a582f4d
bstr156 <bstr156@gmail.com>
2023-06-20 11:54:19 -0500

    testing deployment

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -22,7 +22,6 @@ module.exports = {
     logo: '/assets/logos/logo.png',
     docsDir: 'src',
     docsRepo: 'https://github.com/bstr156/community-hub',
-    base: '/community-hub/',
     docsBranch: 'main',
     lastUpdated: false,
     darkmode: 'disable',
